11b84bebb2a7d9b082dce85f59cae09a013dec94
Michael Ingram <mpingram92@gmail,com>
2016-04-10 21:36:06 -0500

    HAHA CALENDAR WORKS git add -A

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -475,8 +475,7 @@ module.exports = function (grunt) {
     ]);
   });
 
-  grunt.registerTask('server', 'DEPRECATED TASK. Use the "serve" task instead', function (target) {
-    grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
+  grunt.registerTask('s', 'shorthand for grunt serve', function (target) {
     grunt.task.run(['serve:' + target]);
   });
 
