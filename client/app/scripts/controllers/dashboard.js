74d5d509b0414140fa928564ea49c7b906e7fbf7
Michael Ingram <mpingram92@gmail,com>
2016-05-06 16:34:06 -0500

    Fixed issues with fullcalendar population and http get requests not working because of a lack of understanding of asynchronity. Asynchronity is going to kill me or make me smarter.

--- client/app/scripts/controllers/dashboard.js
+++ client/app/scripts/controllers/dashboard.js
@@ -6,34 +6,14 @@
  * @description
  * # MainCtrl
  * Controller of the event_manager dashboard.
- */
+ **/
 
- /**
-  * TODO: conceptually, solve problem of list and calendar in same view.
-  *                 - does list load all events while calendar only loads daily?
-  *                 - no, that's dumb. But how do we make the app responsive?
-  *                 - heuristic: current and future events will be queried more often
-  *                         than past events. So, EM client loads one or two weeks' worth
-  *                         of upcoming events on doc ready and displays them in both views.
-  *                         Now, supposing someone wants to go back in time.... ? 
-  *                                 - why would someone need to do this? 
-  *                                         - to see if recent events happened ok?
-  *                                         - billing?
-  *                                         - feedback from user?
-  *                                         - global review of previous haps?
-  *
-  *                         - ok, so what are the complications of a "show past events" button?
-  *                             - it should load events from the past one or two weeks and display them, no big.
-  *
-  */
 
 angular.module('em_App')
-	// debug: if code doesn't work, getEvents service is the problem
-  .controller('DashboardCtrl', function ($scope, getEvents) {
 
+  .controller('DashboardCtrl', function ($scope, getEvents, fc) {
 
-  	$scope.events = [];
-  	// ok game plan: we call api for single event + buffer of server-defined length;
+	// ok game plan: we call api for single event + buffer of server-defined length;
   	// 
   	// quick notes on search bar behavior: 
   	// 			- searches through buffer and then submits server search request
@@ -42,19 +22,27 @@ angular.module('em_App')
   	// 			- IF no matches in buffer, submit search request
   	// 					- this means that if multiple matches in buffer, search ends at end of buffer.
   	// 			- search request is GET api/events/search?param="string"
-  	// 			- db returns all matches, user then filters through them
-  	// 			- 
+  	// 			- db returns all matches, user then filters through them with |<||>|
+
+    $scope.events = [];
 
     angular.element(document).ready( function () { 
 
 
     	// aquires json event data from server.
-        // TODO: code which calls this method from start of today to end of today.
-        $scope.events.concat( getEvents( $.now() , true) );
+    	// debug: asynchronity is going to either kill me or make me smarter
+        getEvents( $.now(), true, function(data){
+
+        	$scope.events = $scope.events.concat(data);
+        	console.log($scope.events);
+        	fc. initialize ($scope.events, 'dash');
+        });
+        //$scope.events = $scope.events.concat( eventBuffer );
+
 
         // TODO: format events properly for fullcalendar display and for list inclusion.
         // TODO: move to services / elsewhere
-        $('#calendar').fullCalendar({
+        /*$('#calendar').fullCalendar({
             defaultView: 'resourceDay',
             resources: [
                 {'id':'EI',         'name':'EI'},
@@ -76,7 +64,7 @@ angular.module('em_App')
                 {'id':'WSSC 071',   'name':'WSSC 071'},
                 {'id':'CHAS',       'name':'CHAS'}
             ]
-        });
+        }); */
     });
 
 
--- client/app/scripts/services.js
+++ client/app/scripts/services.js
@@ -5,14 +5,127 @@
 var emDashServices = angular.module('emDashServices', []);
 
 
+
+// factory for fullcalendar initialization with default parameters for dashboard
+emDashServices.factory('fc', function() {
+
+	// service instance to be returned
+	var fcService = {};
+
+	// formats event data from server for fc use
+	// TODO: more effectively handle repeating events -
+	// manage on server side? We could generate a repeatingEvents hash id, and give it to
+	// all copies of repeating event. (which would each have a unique mongodb id).
+	// --- fc will be reinitialized on page load - so a hash may not be necessary?
+	//
+	// DEBUG: consequence of this being inside fc is that fc will need to be injected
+	// whenever you need to format data for fc. That sounds like it should be fine, right?
+	fcService.format = function( dbData ){
+		var fcData = {};
+
+		// getting id
+		if (dbData.repeating === true){
+			// TODO: implement!
+			// DEBUG: this might be a terrible idea
+			//fcData.id = dbData.repeatingID;
+			fcData.id = dbData._id;
+
+		} else {
+			fcData.id = dbData._id;
+		}
+
+		// DEBUG: is this how jack did it?
+		fcData.title = 	dbData.name + "\n" + dbData.organizer;
+
+		fcData.start = 	dbData.start;
+		fcData.end =	dbData.end;
+
+		// DEBUG: alternately, respond to onclick event hook
+		fcData.url = 	'events/' + dbData._id;
+
+		return fcData;
+	};
+
+	// initializes fullcalendar with further specific parameters
+	// function takes a string argument indicating name of set of specific parameters
+	fcService.initialize = function( eventBuffer, parameterSet ){
+
+		var that = this;
+
+		// DEBUG:
+		// woah woah woah now, this won't work cowboy. Need to iterate over object the way format is wired rn.
+		// OR JUST SYNCHRONIZE THE DATABASE AND THE FULLCALENDAR VIEW. UGHHHHHHHHH
+		//eventBuffer = this.format(eventBuffer);
+
+		// stores custom parameters to pass to fullcalendar
+		// common params
+		// --------------
+		var params = {
+			resources: [
+	                {'id':'EI',         'name':'EI'},
+	                {'id':'EII',        'name':'EII'},
+	                {'id':'EIII',       'name':'EIII'},
+	                {'id':'EIV',        'name':'EIV'},
+	                {'id':'WIa',        'name':'WIa'},
+	                {'id':'WIb',        'name':'WIb'},
+	                {'id':'WII',        'name':'WII'},
+	                {'id':'WIII',       'name':'WIII'},
+	                {'id':'WIV',        'name':'WIV'},
+	                {'id':'Lobby',      'name':'Lobby'},
+	                {'id':'Library',    'name':'Library'},
+	                {'id':'120',        'name':'120'},
+	                {'id':'129',        'name':'129'},
+	                {'id':'BW11',       'name':'BW11'},
+	                {'id':'BE07',       'name':'BE07'},
+	                {'id':'WSSC 052',   'name':'WSSC 052'},
+	                {'id':'WSSC 071',   'name':'WSSC 071'},
+	                {'id':'CHAS',       'name':'CHAS'}
+	        ],
+
+	        minTime: '07:00:00',
+	        maxTime: '20:00:00',
+
+	        // TODO: fix this bs
+	        // should function here handle logic that if events aren't in eventbuffer, pull data from server?
+	        events: [ that.format(eventBuffer[0]) ]
+	        
+
+
+		};
+
+		// if dashboard environment (DEBUG: or no parameterSet passed)
+		if (parameterSet === 'dash' || parameterSet === 'dashboard' || !parameterSet){
+
+			// set resource view
+			params.defaultView = 'resourceDay';
+
+
+		}
+
+
+		$('#calendar').fullCalendar(params);
+	};
+
+
+	return fcService;
+
+});
+
+
+
 // factory for funtion used by dashboard to acquire events
 // created function takes two inputs:
 // @param string date - ISO 8601 compliant string
 // @param boolean bufferFlag - true if server should return events from upcoming days
 emDashServices.factory('getEvents', ['$http', 
+
 	function($http){
 
-		return function(date, bufferFlag){
+		return function(date, bufferFlag, callback){
+
+			// DEBUG:
+			// ASYNCHRONITYYYYYYYYYYYYYYYYYYYY MY ENEMY
+			var output;
 
 			var requestUrl = 'api/events/' + date;
             // if server should return not only that day's events
@@ -28,24 +141,18 @@ emDashServices.factory('getEvents', ['$http',
             }).then(function successCallback(res) {
 
                 // debug
-                console.log(res.data);
-                
-                return res;
+                // console.log(res.data);
+                console.log('success');
+                callback(res.data);
+                return res.data;
 
             }, function errorCallback(res) {
                 console.debug(res);
                 return 'Response failed!';
             });
 
+			console.log(output);
 		};
+
 }]);
 
-
-// DEBUG: only useful if not using $http service for ajax
-// requests, which like why not
-emDashServices.factory('Event', ['$resource',
-  function($resource){
-    return $resource('api/events', {}, {
-      query: {method:'GET', isArray:true}
-    });
-  }]);
