fadf7a26d0b00f58506a65de83b13cc9401d59c1
Michael Ingram <mpingram92@gmail.com>
2016-11-29 18:20:15 -0600

    Remove web animations polyfill
    - It really wasn't working - I think the open/close event list
    anim is actually super taxing if not native. Oh dear.

--- client/app/main.js
+++ client/app/main.js
@@ -2,6 +2,6 @@
 var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
 var app_module_1 = require('./app.module');
 // polyfill
-require('web-animations-js');
+//import 'web-animations-js';
 platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
 //# sourceMappingURL=main.js.map
--- client/app/main.ts
+++ client/app/main.ts
@@ -2,6 +2,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
 // polyfill
-import 'web-animations-js';
+//import 'web-animations-js';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
