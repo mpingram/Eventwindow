eae76dbfd155b7fbbc7d2b8d057b1bd3d9c42cf2
Michael Ingram <mpingram92@gmail.com>
2016-12-01 20:33:16 -0600

    Switch to hash location route strategy

--- client/app/app.routing.ts
+++ client/app/app.routing.ts
@@ -18,4 +18,4 @@ export const appRoutingProviders: any[] = [
 
 ];
 
-export const routing = RouterModule.forRoot(appRoutes);
+export const routing = RouterModule.forRoot(appRoutes, { useHash: true });
